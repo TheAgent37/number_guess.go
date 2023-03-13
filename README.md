9909a89664fc5fc2b885305abc6cd773afb76716
Burak Necmi "Agent37" KUŞÇU <buraknecmi_kuscu@hotmail.com>
2023-03-13 23:28:31 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# number_guess.go
+
+This is just a basic number guessing game for Go lang.
---
+++ number.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+)
+
+const MAX_GUESS = 3
+const MIN_NUM = 1
+const MAX_NUM = 3
+
+var win bool = false
+
+func guess() {
+	win = false
+	var guess_count int
+	num := rand.Intn(MAX_NUM-MIN_NUM+1) + MIN_NUM
+	fmt.Println("Random number created between", MIN_NUM, "and", MAX_NUM)
+	for guess_count < MAX_GUESS && !win {
+		gnum := ""
+		fmt.Printf("You have %d tries left. Enter your guess: ", MAX_GUESS-guess_count)
+		fmt.Scan(&gnum)
+		n, err := strconv.Atoi(gnum)
+		if err != nil {
+			fmt.Printf("Error! Please enter a number.\n\n")
+			continue
+		}
+		guess_count += 1
+
+		switch {
+		case n > num:
+			fmt.Printf("Your guess is too high.\n\n")
+		case n < num:
+			fmt.Printf("Your guess is too low.\n\n")
+		default:
+			fmt.Printf("You guessed the number (%d) in %d tries!\n\n", num, guess_count)
+			win = true
+		}
+	}
+	if win == false {
+		fmt.Println("You couldn't guess the number!")
+	}
+}
+
+// ! Thıs funciton is not working
+func try_again(win bool) {
+	//var try_loop bool = true
+	// for try_loop {
+	var play string
+	if win {
+		fmt.Printf("Congratulations! Do you want to play again? (y/n): ")
+	} else {
+		fmt.Printf("Do you want to try again? (y/n): ")
+	}
+	fmt.Scan(&play)
+	switch play {
+	case "y":
+		guess()
+	case "n":
+		fmt.Println("Goodbye!")
+	default:
+		fmt.Printf("Invalid input.\n\n")
+		//try_loop = false
+		try_again(win)
+	}
+	//}
+}
+
+func main() {
+	fmt.Printf(`
+Welcome to my number guessing game!
+
+Rules:
+
+-You have %d tries
+-Random generated number is between %d and %d (both including)
+-You can play the game how many as you want!
+
+`, MAX_GUESS, MIN_NUM, MAX_NUM)
+	guess()
+}
